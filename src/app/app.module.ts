2d56c19c09352b09b58d391e3d124a8ce03a249e
Ian Quattlebaum <ian@Ians-iMac.local>
2018-05-29 09:27:25 -0400

    initial setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
+import { ButtonComponent } from './button/button.component';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent,ButtonComponent],
+  imports: [BrowserModule],
+  entryComponents: [ButtonComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const customButton = createCustomElement(ButtonComponent, { injector });
+    customElements.define('a-button', customButton);
+  }
+
+  ngDoBootstrap() {}
+}
